ab6c06881df5c873bc74e96bbe9551eb27ee8be9
davidweichen <124118243+davidweichen@users.noreply.github.com>
2023-03-21 22:10:48 -0700

    Delete IShape.java

--- IShape.java
+++
@@ -1,8 +0,0 @@
-import java.util.Scanner;
-
-public interface IShape {
-    public void define();
-    public String[] draw();
-    public char getposition(int X, int Y);
-    public String[] setborder();
-}
